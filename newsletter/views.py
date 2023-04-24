3eff15859e33944eac2977ea143cb85af52c91ab
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-04-24 12:21:03 +0100

    Serializer and views for newsletter app

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Newsletter, Subscriber
+from .serializers import SubscriberSerializer
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-# Create your views here.
+
+class SubscriberCreateAPIView(generics.CreateAPIView):
+    queryset = Subscriber.objects.all()
+    serializer_class = SubscriberSerializer
+    
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email', None)
+        
+        # if email is not provided do this
+        if not email:
+            return Response({'email': 'This field is required', status=status.HTTP_400_BAD_REQUEST})
+        # it performs a get() operation on d db obj using d parameters 
+        # and if it doesn't exist, it performs a create operation
+        subscriber, created = Subscriber.objects.get_or_create(email=email)
+        
+        if not subscriber.confirmed:
+            subscriber.generate_confirmation_token()
+            # send confirmation email
+            message = Mail(
+                from_email = 'balsacademy@gmail.com',
+                to_emails = email,
+                Subject = 'Confirm your subscription'
+                # google about this, work on this
+                html_content = f'<p>Thank you for signing up for my email newsletter. To confirm your subscription,
+                please <a href='{0}'>click here</a>.</p>'.
+                    format(request.build_absolute_uri('/confirm/{0}/'.
+                                                        format(subscriber.confirmation_token)))
+            )
+            
+            try:
+                sg = SendGridAPIClient('SENDGRID_API_KEY')
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+            except Exception as e:
+                print(e)
+                
+        # subscriber is confirmed
+        return Response('success': 'You have suscessfully subscribed.', status=status.HTTP_201_CREATED)
+    
+class SubscriberDestroyAPIView(generics.DestroyAPIView):
+    queryset = Subscriber.objects.all()
+    serializer_class = SubscriberSerializer
+    lookup_field = 'email'
+    
+    def delete(self, request, *args, **kwargs):
+        # read up on the two
+        email = kwargs.get['email', None]
+        # email = kwargs['email', None]
+        
+        if not email:
+            return Response({'email': 'This field is required', status=status.HTTP_400_BAD_REQUEST})
+        # if email exists
+        subscriber = self.get_object()  # google it
+        subscriber.delete()
+        return Response({'success': 'You have successfully unsubscribed.'})
